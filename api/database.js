e2b329a10403f915640a29626cae3b4ed0750769
Paulo Vitor <paulo19032004@gmail.com>
2021-12-21 08:52:20 -0300

    Update database.js

--- api/database.js
+++ api/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const {Client} = require('pg')
 
-const conectionString = 'postgresql://admin-sys:KZWAV6CGEmdR@yf@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/automative-stetic-5258.defaultdb?sslmode=verify-full'
+const conectionString = process.env.DATABASE_URL
 
 
 const client = new Client(conectionString)
