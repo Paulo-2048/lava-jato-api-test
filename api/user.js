6ad38e536dc85d403e15ca9b93f28211414b17bd
Paulo Vitor <paulo19032004@gmail.com>
2021-12-28 18:56:58 -0300

    Update

--- api/user.js
+++ api/user.js
@@ -7,11 +7,11 @@ router.get("/", async (req, res) => {
   try {
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`User:`);
     let sql = 'SELECT * FROM user_acess'
     con.query(sql, (err, response) => {
     res.send(response['rows'])
     console.log(response)});
+    
   } catch (error) {
     console.error(error);
     return res.status(500).send("Server error");
