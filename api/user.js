16b2312534c5bef03dd2b59af5afcd0e6b7f4268
Paulo Vitor <paulo19032004@gmail.com>
2021-12-28 18:54:43 -0300

    Update

--- api/user.js
+++ api/user.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const con = require('./database')
 
-/**
- * GET product list.
- *
- * @return product list | empty.
- */
+
 router.get("/", async (req, res) => {
   try {
-    res.json({
-      status: 200,
-      message: "Get data has successfully",
-    });
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`User:`);
+    let sql = 'SELECT * FROM user_acess'
+    con.query(sql, (err, response) => {
+    res.send(response['rows'])
+    console.log(response)});
   } catch (error) {
     console.error(error);
     return res.status(500).send("Server error");
