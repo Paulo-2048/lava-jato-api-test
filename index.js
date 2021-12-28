d0c381a5279287673f3a5062243db66c3f2f4b14
Paulo Vitor <paulo19032004@gmail.com>
2021-12-28 18:47:07 -0300

    Update

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.use(express.json({ extended: false }));
 
 app.use("/api/user", user)
 
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+
 
 
 // const app = require('express')();
